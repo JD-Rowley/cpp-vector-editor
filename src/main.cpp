904c23f763419f75b8fc06a9b7a20d21b4f800f8
JD Rowley <jdrowley1989@gmail.com>
2022-05-27 14:03:56 -0600

    added logic for smallest and largest numbers, clearing list

--- src/main.cpp
+++ src/main.cpp
@@ -71,7 +71,7 @@ int main() {
     char selection {};
     int number {};
 
-    vector <int> numbers {100, 200, 300};
+    vector <int> numbers {};
 
     do {
         cout << "P - Print numbers" << endl;
@@ -79,6 +79,7 @@ int main() {
         cout << "M - Display mean of the numbers" << endl;
         cout << "S - Display the smallest number" << endl;
         cout << "L - Display the largest number" << endl;
+        cout << "C - Clear list" << endl;
         cout << "Q - Quit" << endl;
 
         cout << "\nEnter your choice: ";
@@ -88,46 +89,95 @@ int main() {
         switch (selection) {
             case 'p':
             case 'P':
-                cout << "\nPrinting numbers...\n" << endl;
-                if (numbers.size() > 0) {
-                    cout << "[";
-                    for (auto num: numbers) {
-                        cout << " " << num << " ";
+                {
+                    cout << "\nPrinting numbers...\n" << endl;
+                    if (numbers.size() > 0) {
+                        cout << "[";
+                        for (auto num: numbers) {
+                            cout << " " << num << " ";
+                        }
+                        cout << "]\n" << endl;
+                    } else {
+                        cout << "[] - the list is empty\n" << endl;
                     }
-                    cout << "]\n" << endl;
-                } else {
-                    cout << "[] - the list is empty\n" << endl;
                 }
                 break;
             case 'a':
             case 'A':
-                cout << "What number would you like to add? ";
+                {
+                    cout << "What number would you like to add? ";
 
-                cin >> number;
+                    cin >> number;
 
-                cout << "\nAdding " << number << "...\n" << endl;
-                numbers.push_back(number);
+                    cout << "\nAdding " << number << "...\n" << endl;
+                    numbers.push_back(number);
+                }
                 break;
             case 'm':
             case 'M':
-                cout << "\nFinding vector's mean...\n" << endl;
+                {
+                    cout << "\nFinding vector's mean...\n" << endl;
 
-                int total {};
+                    if (numbers.size() > 0) {
+                        int total {};
 
-                for (auto num: numbers) {
-                    total += num;
+                        for (auto num: numbers) {
+                            total += num;
+                        }
+
+                        cout << "The vector's mean is " << static_cast<double>(total)/numbers.size() << "." << endl;
+                    } else {
+                        cout << "Unable to calculate mean - list is empty..." << endl;
+                    }
                 }
-
-                cout << "The vector's mean is " << static_cast<double>(total)/numbers.size() << "." << endl;
-
                 break;
             case 's':
             case 'S':
-                cout << "\nFinding vector's smallest number...\n" << endl;
+                {
+                    cout << "\nFinding vector's smallest number...\n" << endl;
+                    
+                    if (numbers.size() > 0) {
+                        int small_int = numbers.at(0);
+
+                        for (auto i: numbers) {
+                            if (i < small_int) {
+                                small_int = i;
+                            }
+                        }
+
+                        cout << "The vector's smallest element is " << small_int << "." << endl;
+                    } else {
+                        cout << "Cannot determine smallest integer - list is empty..." << endl;
+                    }
+                }
                 break;
             case 'l':
             case 'L':
-                cout << "\nFinding vector's largest number...\n" << endl;
+                {
+                    cout << "\nFinding vector's largest number...\n" << endl;
+
+                    if (numbers.size() > 0) {
+                        int large_int = numbers.at(0);
+
+                        for (auto i: numbers) {
+                            if (i > large_int) {
+                                large_int = i;
+                            }
+                        }
+
+                        cout << "The vector's largest number is " << large_int << "." << endl;
+                    } else {
+                        cout << "Cannot determine largest integer - list is empty..." << endl;
+                    }
+                }
+                break;
+            case 'c':
+            case 'C':
+                {
+                    cout << "\nClearing list...\n" << endl;
+
+                    numbers.clear();
+                }
                 break;
             case 'q':
             case 'Q':
